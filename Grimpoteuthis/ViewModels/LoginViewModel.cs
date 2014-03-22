4ba076b500b195e9d483a6edac5d283b45a4294c
Henrik Andersson <henrik.a.andersson@outlook.com>
2014-03-22 15:48:24 +1000

    Implement GitHub API authentication

--- Grimpoteuthis/ViewModels/LoginViewModel.cs
+++ Grimpoteuthis/ViewModels/LoginViewModel.cs
@@ -1,11 +1,14 @@
 ﻿using ReactiveUI;
 using System;
 using System.Threading.Tasks;
+using Octokit;
+using System.Collections.Generic;
 
 namespace Grimpoteuthis.ViewModels
 {
     public class LoginViewModel : ReactiveObject, IRoutableViewModel
     {
+        IGitHubClient _Client = new GitHubClient(new ProductHeaderValue("Grimpoteuthis"));
         public string UrlPathSegment { get { return "Login"; } }
         public IScreen HostScreen { get; private set; }
 
@@ -35,6 +38,30 @@ namespace Grimpoteuthis.ViewModels
                     (user, pass) => (!String.IsNullOrWhiteSpace(user.Value) && !String.IsNullOrWhiteSpace(pass.Value)));
 
             LoginCommand = new ReactiveCommand(canLogin);
+            LoginCommand.RegisterAsyncTask(async _ =>
+            {
+                await Login();
+            });
+
+        }
+
+        private async Task Login()
+        {
+            _Client.Connection.Credentials = new Credentials(Username, Password);
+
+            var newAuthorization = new NewAuthorization
+            {
+                Scopes = new List<string> { "user", "repo", "delete_repo", "notifications", "gist" },
+                Note = "Grimpoteuthis"
+            };
+
+            var authorization = await _Client.Authorization.GetOrCreateApplicationAuthentication(
+                "client-id-of-your-registered-github-application",
+                "client-secret-of-your-registered-github-application",
+                newAuthorization);
+
+            _Client.Connection.Credentials = new Credentials(authorization.Token);
+            RxApp.MutableResolver.Register(() => _Client, typeof(IGitHubClient));
         }
 
     }
